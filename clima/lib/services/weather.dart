78a4f26a32a85f6c791a2de6774492c0e2e46b85
Kolade Ebenezer <tobakolade@gmail.com>
2023-03-06 03:08:17 +0100

    api-key

--- clima/lib/services/weather.dart
+++ clima/lib/services/weather.dart
@@ -1,30 +1,30 @@
 import '../services/networking.dart';
 import '../services/location.dart';
 
-const api_key = 'e9a336dc55698488726dc979913c8ead';
-const apiSource= 'https://api.openweathermap.org/data/2.5/weather';
-
+const api_key =
+    'api-key'; //register at https://api.openweathermap.org to get a valid api-key
+const apiSource = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
-  Future <dynamic> getanyWeatherData(cityName)async{
-    NetworkHelper networkHelper = NetworkHelper('$apiSource?q=$cityName&units=metric&appid=$api_key');
+  Future<dynamic> getanyWeatherData(cityName) async {
+    NetworkHelper networkHelper =
+        NetworkHelper('$apiSource?q=$cityName&units=metric&appid=$api_key');
     var weatherdata = await networkHelper.getData();
     return weatherdata;
   }
 
-  Future <dynamic> getWeatherData()async{
+  Future<dynamic> getWeatherData() async {
     Location location = Location();
 
     await location.getCurrentPosition();
     print(location.longitude);
     print(location.latitude);
 
-    NetworkHelper networkHelper = NetworkHelper('$apiSource?lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=$api_key');
+    NetworkHelper networkHelper = NetworkHelper(
+        '$apiSource?lat=${location.latitude}&lon=${location.longitude}&units=metric&appid=$api_key');
     var weatherdata = await networkHelper.getData();
     return weatherdata;
-
   }
-
 
   String getWeatherIcon(int condition) {
     if (condition < 300) {
